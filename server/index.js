f651273c8df450688260a2dfb456deee6de2f935
Chris De montigny <chrisdemontigny@yahoo.com>
2021-08-24 19:07:22 -0600

    C:/Program Files/Git/api/posts endpoint working

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { SERVER_PORT,CONNECTION_STRING,SESSION_SECRET} = process.env
 
 const express = require('express');
 const userCtrl = require('./controllers/user');
-// const postCtrl = require('./controllers/posts');
+const postCtrl = require('./controllers/posts');
 const session = require('express-session');
 const bcrypt = require('bcryptjs')
 
@@ -23,11 +23,12 @@ app.use(session({
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
 app.post('/api/auth/login', userCtrl.login);
-// app.get('/api/auth/me', userCtrl.getUser);
+app.get('/api/auth/me', userCtrl.getUser);
 app.post('/api/auth/logout', userCtrl.logout);
 
 // //Post Endpoints
-// app.get('/api/posts', postCtrl.readPosts);
+// test end points http://localhost:4000/api/posts?mine=6&search=new
+app.get('/api/posts', postCtrl.readPosts);
 // app.post('/api/post', postCtrl.createPost);
 // app.get('/api/post/:id', postCtrl.readPost);
 // app.delete('/api/post/:id', postCtrl.deletePost)
